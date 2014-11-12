dc4041b5a45d6e972ed0892a5fa21d02fc682f6b
Fuat ŞENGÜL <fuat@fuatsengul.com.tr>
2014-11-12 19:43:27 +0200

    added below option

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     summary: "leaflet-textpath is allow you to add texts on paths.",
-    version: "1.0.0",
+    version: "1.1.0",
     git: "https://github.com/fuatsengul/leafelet-textpath"
     
 });
 
 Package.on_use(function(api){
-    api.versionsFrom('METEOR@0.9.0.1');
+    api.versionsFrom('1.0');
     api.use([
     'mrt:leaflet@0.3.8', 'session@1.0.0', 'underscore@1.0.0'
     ], 'client');
